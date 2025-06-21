ea23c957977a3e9af0f8ea25be9bf1f41e04d46e
jesus <jesusramondiaz11@gmail.com>
2025-06-21 03:37:17 -0400

    chore: Add environment debug script

---
+++ debug.php
@@ -0,0 +1,12 @@
+<?php
+echo "<pre>";
+
+echo "--- getenv() ---\n";
+print_r(getenv());
+echo "\n--- \$_ENV ---\n";
+print_r($_ENV);
+echo "\n--- \$_SERVER ---\n";
+print_r($_SERVER);
+
+echo "</pre>";
+?> 
